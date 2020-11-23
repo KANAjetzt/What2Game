d470d4eb6a1ee61c850efcb4dd5250c7a9b526bc
KANAjetzt <kai@kana.jetzt>
2020-11-23 16:45:24 +0100

    ✔ auth function working?!

--- functions/auth/auth.js
+++ functions/auth/auth.js
@@ -15,20 +15,12 @@ passport.deserializeUser(function (obj, done) {
 passport.use(
   new SteamStrategy(
     {
-      returnURL: `${process.env.BASE_URL}/auth/steam/return`,
+      returnURL: `${process.env.BASE_URL}api/auth/steam/return`,
       realm: `${process.env.BASE_URL}`,
       apiKey: `${process.env.STEAM_API_KEY}`,
     },
     function (identifier, profile, done) {
-      // User.findByOpenID({ openId: identifier }, function (err, user) {
-      //   return done(err, user);
-      // });
-
       process.nextTick(function () {
-        // To keep the example simple, the user's Steam profile is returned to
-        // represent the logged-in user.  In a typical application, you would want
-        // to associate the Steam account with a user record in your database,
-        // and return that user instead.
         profile.identifier = identifier;
         return done(null, profile);
       });
@@ -38,39 +30,16 @@ passport.use(
 
 const app = express() // You can also use Express
   app
-  .use((req, res, next) => {
-    console.log('something')
-    next()
-  })
   .use(passport.initialize())
   .use(passport.session())
-  .get("/api/auth",(req, res, next) => {console.log('hellow!'); next()}, passport.authenticate("steam", { failureRedirect: "/" }, (err) => {
-    console.log('test')
-    console.log(err)
-  }))
+  .get("/api/auth", passport.authenticate("steam", { failureRedirect: "/" }))
   .get(
-    "/auth/steam/return",
-    (req, res, next) => {console.log('hello back!'); next()},
+    "/api/auth/steam/return",
     passport.authenticate("steam", { failureRedirect: "/" }),
-    (err, req, res) => {
-      console.log('test test')
-      console.log(err)
+    (req, res) => {
       const steamId = res.req.user.id;
-      console.log("yes yes?!")
       res.redirect(`/?steamID=${steamId}`);
-
-      /* Returns steamID */
-      // res.status(200).json({--
-      //   success: true,
-      //   data: {
-      //     steamId,
-      //   },
-      // });
     }
   )
-  // .get(`/auth/steam/return`, passport.authenticate("steam", { failureRedirect: "/" }, (req, res) => {
-  //   const steamId = res.req.user.id;
-  //   console.log(steamId)
-  // }))
 
 module.exports.handler = serverless(app);
